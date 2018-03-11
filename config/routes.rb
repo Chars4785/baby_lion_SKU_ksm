c01b8c71f219872f0b5039583ca8ee08370697e6
Seongmin <doodit@likelion.org>
2018-03-11 10:59:13 +0000

    For auto deploy script

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   get 'posts/edit/:id' => 'posts#edit'
   
   post 'posts/update/:id' => 'posts#update'
-  #get 'posts/update/:id' => 'posts#update'
+ 
 
 
   delete 'posts/destroy/:id' => 'posts#destroy'
@@ -25,13 +25,7 @@ Rails.application.routes.draw do
   post 'comments/create' => 'comments#create'
 
   post 'comments/destroy/:id' => 'comments#destroy'
-  #delete 'comments/destroy/:id' => 'comments#destroy'
-  
-  
-  # post 'posts/like_up/:id' => 'posts#like_up'
-  # post 'posts/like_dn/:id' => 'posts#like_dn'
-  # post 'posts/like_create/:id' => 'posts#like_create'
-  
+ 
   
   post 'posts/:post_id/like' => 'posts#like_toggle'
   
